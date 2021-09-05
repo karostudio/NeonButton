07e124b4d406131f421ffef50bf13d5c8c161f2b
Amin Rahimzadeh <aminworkapp@gmail.com>
2021-09-05 16:22:11 +0430

    Setter function for attributes created

--- app/src/main/java/studio/karo/example/neonbutton/MainActivity.kt
+++ app/src/main/java/studio/karo/example/neonbutton/MainActivity.kt
@@ -1,16 +1,14 @@
 package studio.karo.example.neonbutton
 
-import android.content.res.ColorStateList
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.ContextCompat
-import studio.karo.neonbutton.NeonButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
     }
 }
